cac46815089551e40c02e68e0c02354e37799bff
孙世康 <50051815+Ericsunsk@users.noreply.github.com>
2026-08-19 16:16:38 +0800

    style: rename collection hero headline to GLAMOURIA

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -75,7 +75,7 @@
 		<h1
 			class="font-display text-[12vw] md:text-[14vw] font-bold tracking-[0.05em] leading-none text-red-600 select-none whitespace-nowrap"
 		>
-			ELEMENTHIC
+			GLAMOURIA
 		</h1>
 	</div>
 
